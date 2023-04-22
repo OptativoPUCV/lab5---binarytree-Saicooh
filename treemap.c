442505b09ffc38428cc7f05b03dcd6de069438f4
Replit user <>
2023-04-22 00:50:53 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -26,20 +26,26 @@ int is_equal(TreeMap* tree, void* key1, void* key2){
 }
 
 
-TreeNode * createTreeNode(void* key, void * value) {
-    TreeNode * new = (TreeNode *)malloc(sizeof(TreeNode));
-    if (new == NULL) return NULL;
-    new->pair = (Pair *)malloc(sizeof(Pair));
-    new->pair->key = key;
-    new->pair->value = value;
-    new->parent = new->left = new->right = NULL;
-    return new;
+TreeNode *createTreeNode(void *key, void *value)
+{
+  TreeNode *new = malloc(sizeof(TreeNode));
+  Pair *pair = (Pair*) malloc(sizeof(Pair));
+  if (new == NULL) return NULL;
+  new -> pair -> key = key;
+  new -> pair -> value = value;
+  new -> parent = new -> left = new -> right = NULL;
+    
+  return new;
 }
 
-TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
+TreeMap *createTreeMap(int (*lower_than) (void *key1, void *key2))
+{
+  TreeMap *mapa = malloc(sizeof(TreeMap));
+  mapa -> root == NULL;
+  mapa -> current == NULL;
+  mapa -> lower_than = lower_than;
 
-    //new->lower_than = lower_than;
-    return NULL;
+  return mapa;
 }
 
 
